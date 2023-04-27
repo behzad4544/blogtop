8f92ec2e6c9f362de47ccc87ba8c15464aa9e004
behzad <behzad.kermanii@gmail.com>
2023-04-27 13:12:47 +0330

    theme added

---
+++ functions/helpers.php
@@ -0,0 +1,17 @@
+<?php
+define('BASE_URL', 'http://localhost/blogtop/');
+function redirect($path)
+{
+    header("Location:" . trim(BASE_URL, '/ ') . "/" . trim($path, '/ '));
+    exit;
+}
+function asset($file)
+{
+    return trim(BASE_URL, '/ ') . '/' . trim($file, '/ ');
+}
+function dd($var)
+{
+    echo "<pre>";
+    var_dump($var);
+    exit;
+}
